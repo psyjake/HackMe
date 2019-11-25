1c42a27429e18f3f1f7fcbf25c5c1363bf3e6252
psyjb12 <psyjb12@nottingham.ac.uk>
2019-11-25 12:17:16 +0000

    Created new package for game logic.

---
+++ src/hackme/gamelogic/GameEngine.java
@@ -0,0 +1,14 @@
+package hackme.gamelogic;
+
+import java.util.List;
+
+public class GameEngine {
+    /**
+     * Returns a random list of words from the wordbank.
+     * @param wordLength The desired length of the word, should be determined by the current difficulty
+     * @return a list of randomly selected words of the correct length.
+     */
+    List<String> getWordList(int wordLength) {
+        return null;
+    }
+}
