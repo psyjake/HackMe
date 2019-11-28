0edb92daf7e834d58266100f7cc9acbcd920d2c5
psyjb12 <psyjb12@nottingham.ac.uk>
2019-11-28 10:28:00 +0000

    Naively reading words from txt files

--- src/hackme/gamelogic/GameEngine.java
+++ src/hackme/gamelogic/GameEngine.java
@@ -1,14 +1,50 @@
 package hackme.gamelogic;
 
+import java.io.*;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class GameEngine {
+
+    public enum Difficulty {
+        EASY, MODERATE, HARD
+    }
+
     /**
      * Returns a random list of words from the wordbank.
-     * @param wordLength The desired length of the word, should be determined by the current difficulty
+     *
+     * @param difficulty The current difficulty, this determines the length of retrieved words
      * @return a list of randomly selected words of the correct length.
      */
-    public List<String> getWordList(int wordLength) {
-        return null;
+    public List<String> getWordList(Difficulty difficulty) {
+        Scanner wordScanner = getWordFileScanner(difficulty);
+        ArrayList<String> wordList = new ArrayList<>();
+        while (wordScanner.hasNextLine() && wordList.size() < 10) {
+            wordList.add(wordScanner.nextLine());
+        }
+        return wordList;
+        //TODO read random words, not first 10.
+    }
+
+    //TODO properly read each file
+    private Scanner getWordFileScanner(Difficulty difficulty) {
+        File wordFile;
+        switch (difficulty) {
+            case EASY:
+                wordFile = new File(getClass().getResource("EasyWords.txt").getPath());
+            case MODERATE:
+                wordFile = new File(getClass().getResource("ModerateWords.txt").getPath());
+            case HARD:
+                wordFile = new File(getClass().getResource("HardWords.txt").getPath());
+            default:
+                wordFile = new File(getClass().getResource("EasyWords.txt").getPath());
+        }
+        try {
+            return new Scanner(wordFile);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("Invalid File URI", e);
+        }
     }
 }
--- src/hackme/gamelogic/GameEngineTest.java
+++ src/hackme/gamelogic/GameEngineTest.java
@@ -8,8 +8,8 @@ import static org.junit.jupiter.api.Assertions.*;
 class GameEngineTest {
 
     private GameEngine gameEngine;
-    private int numberOfWords;
-    private int sizeOfWords;
+    private int numberOfWords = 10;
+    private int sizeOfWords = 7;
 
     @org.junit.jupiter.api.BeforeEach
     void setUp() {
@@ -20,26 +20,24 @@ class GameEngineTest {
     void tearDown() {
         gameEngine = null;
         numberOfWords = 10;
-        sizeOfWords = 10;
+        sizeOfWords = 5;
     }
 
     @org.junit.jupiter.api.Test
     void testNumberOfWords() {
-        List<String> words = gameEngine.getWordList(sizeOfWords);
+        List<String> words = gameEngine.getWordList(GameEngine.Difficulty.MODERATE);
         assertEquals(numberOfWords, words.size());
     }
 
     @org.junit.jupiter.api.Test
     void testSizeOfWords() {
-        List<String> words = gameEngine.getWordList(sizeOfWords);
+        List<String> words = gameEngine.getWordList(GameEngine.Difficulty.MODERATE);
         Iterator<String> itrWords = words.iterator();
 
-        do
-        {
-            if(itrWords.next().length() != 10)
-            {
-                fail("not all words have a length of 10");
+        do {
+            if (itrWords.next().length() != sizeOfWords) {
+                fail("not all words have a length of " + sizeOfWords);
             }
-        }while(itrWords.hasNext());
+        } while (itrWords.hasNext());
     }
 }
