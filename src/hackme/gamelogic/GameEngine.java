fc282dbc4c9a7ee4057de07eeebbdf9ef272f11f
psyjb12 <psyjb12@nottingham.ac.uk>
2019-11-28 12:51:20 +0000

    Reading words from txt files.

--- src/hackme/gamelogic/GameEngine.java
+++ src/hackme/gamelogic/GameEngine.java
@@ -2,7 +2,10 @@ package hackme.gamelogic;
 
 import java.io.*;
 import java.net.URL;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
@@ -16,31 +19,46 @@ public class GameEngine {
      * Returns a random list of words from the wordbank.
      *
      * @param difficulty The current difficulty, this determines the length of retrieved words
-     * @return a list of randomly selected words of the correct length.
+     * @return a list of randomly selected words of the correct length
      */
     public List<String> getWordList(Difficulty difficulty) {
         Scanner wordScanner = getWordFileScanner(difficulty);
+        ArrayList<String> allWords = new ArrayList<>();//A list to contain all words from file.
+        while (wordScanner.hasNextLine()) {
+            allWords.add(wordScanner.nextLine());
+        }
+        Collections.shuffle(allWords);//Randomise the order of the list.
         ArrayList<String> wordList = new ArrayList<>();
-        while (wordScanner.hasNextLine() && wordList.size() < 10) {
-            wordList.add(wordScanner.nextLine());
+
+        //Add the first ten elements to the final list.
+        for (int i = 0; i < 10; i++) {
+            wordList.add(allWords.get(i));
         }
         return wordList;
-        //TODO read random words, not first 10.
     }
 
-    //TODO properly read each file
+    /**
+     * Create a Scanner object for the appropriate word bank file
+     *
+     * @param difficulty The current difficulty of the game (to determine which file to load)
+     * @return Scanner object that can be used to iterate over the right file
+     */
     private Scanner getWordFileScanner(Difficulty difficulty) {
         File wordFile;
+        String path = System.getProperty("user.dir") + "\\src\\hackme\\resources\\";
         switch (difficulty) {
-            case EASY:
-                wordFile = new File(getClass().getResource("EasyWords.txt").getPath());
             case MODERATE:
-                wordFile = new File(getClass().getResource("ModerateWords.txt").getPath());
+                path += "ModerateWords.txt";
+                break;
             case HARD:
-                wordFile = new File(getClass().getResource("HardWords.txt").getPath());
-            default:
-                wordFile = new File(getClass().getResource("EasyWords.txt").getPath());
+                path  += "HardWords.txt";
+                break;
+            default://If 'easy' or erroneous, it will load EasyWords.
+                path += "EasyWords.txt";
+                break;
         }
+        System.out.println(path);
+        wordFile = new File(path);
         try {
             return new Scanner(wordFile);
         } catch (FileNotFoundException e) {
--- src/hackme/gamelogic/GameEngineTest.java
+++ src/hackme/gamelogic/GameEngineTest.java
@@ -8,30 +8,47 @@ import static org.junit.jupiter.api.Assertions.*;
 class GameEngineTest {
 
     private GameEngine gameEngine;
+    private GameEngine.Difficulty gameDifficulty = GameEngine.Difficulty.HARD;
     private int numberOfWords = 10;
     private int sizeOfWords = 7;
 
     @org.junit.jupiter.api.BeforeEach
     void setUp() {
         gameEngine = new GameEngine();
+        switch(gameDifficulty) {
+            case EASY:
+                sizeOfWords = 5;
+                break;
+            case MODERATE:
+                sizeOfWords = 7;
+                break;
+            case HARD:
+                sizeOfWords = 9;
+                break;
+        }
     }
 
     @org.junit.jupiter.api.AfterEach
     void tearDown() {
         gameEngine = null;
-        numberOfWords = 10;
-        sizeOfWords = 5;
+//        numberOfWords = 10;
+//        sizeOfWords = 5;
     }
 
     @org.junit.jupiter.api.Test
     void testNumberOfWords() {
-        List<String> words = gameEngine.getWordList(GameEngine.Difficulty.MODERATE);
+        List<String> words = gameEngine.getWordList(gameDifficulty);
+        for (String word : words) {
+            System.out.println(word);
+        }
         assertEquals(numberOfWords, words.size());
+
+
     }
 
     @org.junit.jupiter.api.Test
     void testSizeOfWords() {
-        List<String> words = gameEngine.getWordList(GameEngine.Difficulty.MODERATE);
+        List<String> words = gameEngine.getWordList(gameDifficulty);
         Iterator<String> itrWords = words.iterator();
 
         do {
